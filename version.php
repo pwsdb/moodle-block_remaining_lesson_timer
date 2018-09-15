14e13abe55584b19dde0935af701aed808898d12
Greg Smith <41300287+pwsdb@users.noreply.github.com>
2018-09-14 23:52:46 -0400

    Create version.php

---
+++ version.php
@@ -0,0 +1,29 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version details
+ *
+ * @package    block_remaining_lesson_timer
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  2011 onwards Greg Smith, Pompano Beach, USA
+ */ 
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2018051700;      // YYYYMMDDHH (Moodle 3.5+)
+$plugin->requires  = 2010112400;      // YYYYMMDDHH (Moodle 2.0+) 
+$plugin->component = 'block_remaining_lesson_timer'; 
