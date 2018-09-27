8ee22db4b90e9ae8c90ca7b11cceb1e72ee946ad
Greg Smith <41300287+pwsdb@users.noreply.github.com>
2018-09-26 23:56:59 -0400

    Add files via upload

--- edit_form.php
+++ edit_form.php
@@ -23,6 +23,9 @@
  * @author     Greg Smith, Florida
  */
 
+/**
+ * Form for editing remaining lesson timer countdown block instances.
+ */
 class  block_timer_countdown_edit_form  extends block_edit_form {
 
     protected function specific_definition($mform) {
