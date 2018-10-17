f8fdc5c077e7d982e826f29d8a00ddc0cf3de5c3
Greg Smith <41300287+pwsdb@users.noreply.github.com>
2018-10-16 22:33:34 -0400

    Add files via upload

--- lang/en/block_remaining_lesson_timer.php
+++ lang/en/block_remaining_lesson_timer.php
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-/*
+/**
  * Block for displaying the elapsed and remaining time in a timed lesson
  *
  * @package    block_remaining_lesson_timer
